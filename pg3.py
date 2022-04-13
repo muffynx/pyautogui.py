48377803a6081044505026ef587dcaec813349ac
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2022-04-13 16:25:41 +0700

    Add files via upload

--- pg3.py
+++ pg3.py
@@ -1,4 +1,8 @@
 import pyautogui as pg
+import time
 pg.hotkey('ctrl','shift','t')
 pg.write('sudo apt update && sudo apt upgrade -y')
 pg.press('enter')
+pg.write('muffyn')
+pg.press('enter')
+pg.sleep(100)
