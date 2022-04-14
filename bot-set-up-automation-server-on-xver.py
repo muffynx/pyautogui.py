20bf2fb83c60b5260c621e67afdb66e88bc13697
muffyn_pp <72650070+muffynx@users.noreply.github.com>
2022-04-14 19:41:44 +0700

    Add files via upload

---
+++ bot-set-up-automation-server-on-xver.py
@@ -0,0 +1,23 @@
+
+#you must login on my.xver before user
+import time
+import pyautogui as pg
+pg.hotkey('win','r')
+pg.write('chrome')
+pg.hotkey('enter')
+time.sleep(1)
+pg.write("https://my.xver.cloud")
+pg.press('enter')
+pg.press('f11')
+time.sleep(1)
+pg.click(1250,110)
+time.sleep(1)
+pg.click(625,315)
+pg.hotkey('down')
+pg.hotkey('enter')
+time.sleep(1)
+pg.click(638,450)
+pg.hotkey('tab')
+pg.write('Muffyn29300.')
+pg.hotkey('tab')
+pg.write('muffyn1')
